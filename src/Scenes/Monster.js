72162ffe563145b42dcd73e172b59a6f509a9d65
nilesCSVR <156045577+nilesCSVR@users.noreply.github.com>
2025-04-25 13:42:45 -0700

    Add files via upload

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -1,45 +1,83 @@
 class Monster extends Phaser.Scene {
     constructor() {
         super("monsterScene");
-        this.my = {sprite: {}};  // Create an object to hold sprite bindings
-
-        //Create constants for the monster location
+        this.my = { sprite: {} };
         this.bodyX = 300;
         this.bodyY = 350;
-        
+        this.speed = 2;
     }
 
-    // Use preload to load art and sound assets before the scene starts running.
     preload() {
-        // Assets from Kenny Assets pack "Monster Builder Pack"
-        // https://kenney.nl/assets/monster-builder-pack
         this.load.setPath("./assets/");
-
-        // Load sprite atlas
         this.load.atlasXML("monsterParts", "spritesheet_default.png", "spritesheet_default.xml");
-        
-        // update instruction text
-        document.getElementById('description').innerHTML = '<h2>Monster.js<br>S - smile // F - show fangs<br>A - move left // D - move right</h2>'
+
+        document.getElementById('description').innerHTML = 
+        '<h2>Monster.js<br>S - smile // F - show fangs<br>A - move left // D - move right</h2>';
     }
 
     create() {
-        let my = this.my;   // create an alias to this.my for readability
-
-        // Create the main body sprite
-        //
-        // this.add.sprite(x,y, "{atlas key name}", "{name of sprite within atlas}")
-        //
-        // look in spritesheet_default.xml for the individual sprite names
-        // You can also download the asset pack and look in the PNG/default folder.
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
+        let my = this.my;
 
         
+        // Legs
+        my.sprite.legL = this.add.sprite(this.bodyX - 50, this.bodyY + 130, "monsterParts", "leg_blueA.png");
+        my.sprite.legR = this.add.sprite(this.bodyX + 50, this.bodyY + 130, "monsterParts", "leg_blueA.png");
+        my.sprite.legL.flipX = true;
+
+        // Arms
+        my.sprite.armL = this.add.sprite(this.bodyX - 100, this.bodyY + 80, "monsterParts", "arm_blueD.png");
+        my.sprite.armR = this.add.sprite(this.bodyX + 100, this.bodyY + 80, "monsterParts", "arm_blueD.png");
+        my.sprite.armL.flipX = true;
+
+        // Monster body
+        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_blueB.png");
+
+        // Eye
+        my.sprite.eye = this.add.sprite(this.bodyX, this.bodyY - 50, "monsterParts", "eye_cute_dark.png");
+
+        // Mouths
+        my.sprite.mouthSmile = this.add.sprite(this.bodyX, this.bodyY + 30, "monsterParts", "mouth_closed_happy.png");
+        my.sprite.mouthFangs = this.add.sprite(this.bodyX, this.bodyY + 30, "monsterParts", "mouth_closed_fangs.png");
+        my.sprite.mouthFangs.visible = false; 
+
+        // Accessories
+        my.sprite.antennaR = this.add.sprite(this.bodyX - 40, this.bodyY - 110, "monsterParts", "detail_blue_antenna_large.png");
+        my.sprite.antennaL = this.add.sprite(this.bodyX + 40, this.bodyY - 110, "monsterParts", "detail_blue_antenna_large.png");
+        my.sprite.antennaR.flipX = true;
+
+        // Keyboard input
+        this.cursors = this.input.keyboard.addKeys({
+            left: 'A',
+            right: 'D',
+            smile: 'S',
+            fangs: 'F'
+        });
     }
 
     update() {
-        let my = this.my;    // create an alias to this.my for readability
+        let my = this.my;
 
-       
+        // smiles/fang keyboard inputs
+        if (Phaser.Input.Keyboard.JustDown(this.cursors.smile)) {
+            my.sprite.mouthSmile.visible = true;
+            my.sprite.mouthFangs.visible = false;
+        } else if (Phaser.Input.Keyboard.JustDown(this.cursors.fangs)) {
+            my.sprite.mouthSmile.visible = false;
+            my.sprite.mouthFangs.visible = true;
+        }
+
+        // moving left and right
+        let moveX = 0;
+        if (this.cursors.left.isDown) {
+            moveX = -this.speed;
+        } else if (this.cursors.right.isDown) {
+            moveX = this.speed;
+        }
+
+        if (moveX !== 0) {
+            for (let part in my.sprite) {
+                my.sprite[part].x += moveX;
+            }
+        }
     }
-
 }
